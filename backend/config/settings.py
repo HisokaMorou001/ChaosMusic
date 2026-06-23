25de6b49aae173505bc504c9f8065453b4302366
HisokaMorou001 <ohsasukemc@gmail.com>
2026-06-23 09:52:57 +0200

    Risistemati i settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,9 +79,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-import os
-import dj_database_url
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
